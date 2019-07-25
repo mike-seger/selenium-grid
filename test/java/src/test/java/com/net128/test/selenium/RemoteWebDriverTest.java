929b062657f8affbbf25419b775cee9b2d951c0f
Michael Seger <michael@macbookpro>
2019-07-25 22:54:46 +0200

    proper maven and java identifiers

--- test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
+++ test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
@@ -1,24 +1,19 @@
-package com.net128.test.webdriver;
+package com.net128.test.selenium;
 
 import junit.framework.TestCase;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.*;
 import java.net.URL;
 
-public class WebDriverTest extends TestCase {
+public class RemoteWebDriverTest extends TestCase {
     private Capabilities chromeCapabilities = DesiredCapabilities.chrome();
     private Capabilities firefoxCapabilities = DesiredCapabilities.firefox();
     private RemoteWebDriver chrome;
     private RemoteWebDriver firefox;
 
     public void setUp() throws Exception {
-        chrome = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
-        firefox = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
-
-/*        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().setPosition(new Point(220, 10));
-	    driver.manage().window().setSize(new Dimension(1000,650));
-  */
+        chrome = new RemoteWebDriver(new URL("http://server1:4444/wd/hub"), chromeCapabilities);
+        firefox = new RemoteWebDriver(new URL("http://server1:4444/wd/hub"), firefoxCapabilities);
     }
 
     public void testChrome() {
