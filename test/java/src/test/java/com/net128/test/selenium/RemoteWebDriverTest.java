31e1d2c7cb0e7a92624598f882cef8520d04fe7e
Michael Seger <michael@macbookpro>
2019-07-25 23:54:09 +0200

    cleaner java selenium

--- test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
+++ test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
@@ -2,18 +2,20 @@ package com.net128.test.selenium;
 
 import junit.framework.TestCase;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.*;
 import java.net.URL;
 
 public class RemoteWebDriverTest extends TestCase {
-    private Capabilities chromeCapabilities = DesiredCapabilities.chrome();
-    private Capabilities firefoxCapabilities = DesiredCapabilities.firefox();
+    private ChromeOptions chromeOptions = new ChromeOptions();
+    private FirefoxOptions firefoxOptions = new FirefoxOptions();
     private RemoteWebDriver chrome;
     private RemoteWebDriver firefox;
 
     public void setUp() throws Exception {
-        chrome = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
-        firefox = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
+        chrome = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+        firefox = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxOptions);
     }
 
     public void testChrome() {
