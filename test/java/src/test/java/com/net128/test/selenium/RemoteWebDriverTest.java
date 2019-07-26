07b56a960509b251ce6f8e80a8b1956ec6a2def5
Michael Seger <michael@macbookpro>
2019-07-27 00:36:41 +0200

    added screenshot example

--- test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
+++ test/java/src/test/java/com/net128/test/selenium/RemoteWebDriverTest.java
@@ -2,14 +2,21 @@ package com.net128.test.selenium;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import junit.framework.TestCase;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.HashMap;
 
 public class RemoteWebDriverTest extends TestCase {
@@ -18,33 +25,61 @@ public class RemoteWebDriverTest extends TestCase {
     private RemoteWebDriver chrome;
     private RemoteWebDriver firefox;
     private HashMap<String, String> configuration;
+    private String screenshotDir;
 
     public void setUp() throws Exception {
-        ObjectMapper mapper=new ObjectMapper();
-        String configName = "configuration.json";
-        configuration = mapper.readValue(getClass()
-            .getResource("/"+configName), HashMap.class);
-        try (FileInputStream fis=new FileInputStream(configName))
-            { mapper.readerForUpdating(configuration).readValue(fis); }
-        catch(FileNotFoundException e) {}
-        catch(Exception e) { e.printStackTrace(); }
+        configuration=loadConfiguration();
+
+        File dir=new File(configuration.get("screenshotDestination"));
+        dir.mkdirs();
+        screenshotDir=dir.getAbsolutePath();
+
         chrome = new RemoteWebDriver(new URL(configuration.get("hubBaseUrl")+"/wd/hub"), chromeOptions);
         firefox = new RemoteWebDriver(new URL(configuration.get("hubBaseUrl")+"/wd/hub"), firefoxOptions);
     }
 
-    public void testChrome() {
+    public void testChrome() throws IOException {
         chrome.get(configuration.get("homePage"));
         assertEquals(configuration.get("expectedTitle"), chrome.getTitle());
+        takeScreenshot(chrome, configuration.get("chrome"));
     }
 
-    public void testFirefox() {
+    public void testFirefox() throws IOException {
         firefox.get(configuration.get("homePage"));
         assertEquals(configuration.get("expectedTitle"), firefox.getTitle());
+        takeScreenshot(firefox, configuration.get("firefox"));
     }
 
     public void tearDown() {
         chrome.quit();
         firefox.quit();
+    }
+
+    private HashMap<String, String> loadConfiguration() throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+        String configName = "configuration.json";
+        @SuppressWarnings("unchecked")
+        HashMap<String, String> configuration = mapper.readValue(getClass()
+            .getResource("/"+configName), HashMap.class);
+        try (FileInputStream fis=new FileInputStream(configName))
+        { mapper.readerForUpdating(configuration).readValue(fis); }
+        catch(FileNotFoundException e) {}
+        catch(Exception e) { e.printStackTrace(); }
+        return configuration;
+    }
+
+    private File takeScreenshot(RemoteWebDriver driver, String namePrefix) throws IOException {
+        Augmenter augmenter = new Augmenter();
+        TakesScreenshot ts = (TakesScreenshot) augmenter.augment(driver);
+        File file = ts.getScreenshotAs(OutputType.FILE);
+        String destName=namePrefix + "-" +
+            Instant.now().toString().replaceAll("[:.-]", "")+".png";
+        File destFile=new File(screenshotDir, destName);
+        Files.move(Paths.get(file.getAbsolutePath()), Paths.get(destFile.getAbsolutePath()));
+        if(!destFile.exists()) {
+            throw new RuntimeException("Could move screenshot from: "+file+" to "+destFile);
+        }
+        return destFile;
     }
 }
 
